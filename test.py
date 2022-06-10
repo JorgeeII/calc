6d9d034692c21eec5ee6ec6776d2060a8ddda2f9
JorgeeII <94064448+JorgeeII@users.noreply.github.com>
2022-06-10 18:50:42 +0200

    First Commit

---
+++ test.py
@@ -0,0 +1,13 @@
+class Kalkulacka:
+    @classmethod
+    def secti(cls, a, b):
+        return a + b
+
+
+def main():
+
+    print(Kalkulacka.secti(5, 6))
+
+
+if __name__ == "__main__":
+    main()
